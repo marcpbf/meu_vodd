0c57be0c721b7d6f98ebb67b3a9bd71c254f40c0
marcpbf <mario_cpbf@hotmail.com>
2026-02-07 21:34:39 -0300

    uploud do arquivo php

---
+++ player_api.php
@@ -0,0 +1,24 @@
+{
+  "user_info": {
+    "username": "demo",
+    "password": "1234",
+    "message": "Welcome",
+    "status": "Active",
+    "auth": 1,
+    "is_trial": 0,
+    "active_cons": 0,
+    "created_at": "1700000000",
+    "exp_date": "1999999999",
+    "max_connections": 1,
+    "allowed_output_formats": ["m3u8", "mp4"]
+  },
+  "server_info": {
+    "url": "SEU_USUARIO.github.io",
+    "port": "443",
+    "https_port": "443",
+    "server_protocol": "https",
+    "rtmp_port": "0",
+    "timezone": "America/Sao_Paulo",
+    "timestamp_now": "1700000000"
+  }
+}
